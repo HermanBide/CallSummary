0c8ab5b97250bda18a60cf4a4b88dff6aadd0625
hermanb <bideherman@yahoo.com>
2023-04-18 18:42:45 -0700

    updated object to csv

--- index.js
+++ index.js
@@ -8,55 +8,49 @@ function testFileRead() {
       return;
     }
     // console.log(`The Shape of Data ${ data} \n The data as below : `)
-
-    // const rows = data
-    //   .split("\n")
-    //   .map((row) => row.trim().split("\s+"))
-    //   .filter((row) => row != "");
-    //   console.log(rows)
-
-    const rows = data.split("\n")
-
+    const rows = data.split("\n");
 
     function test_String(num, characters, row) {
-        for(let i = 0; i < num.length; i++){
-            if(characters.some((element) => element == row.substring(0, num[i]), row)) {
-                return true;
-            }
-        }
-         return false 
-    }
-
-    let obj ={};
-    let key = [];
-    let value = [];
-    
-    for (let i = 0; i < rows.length; i++) {
-        let row = rows[i];
-        // return row
-        if (row) {
-            if(!test_String([4,5,6], ["Ext:", "Name:", "SYNTEL", "----"], row)) {
-                console.log(row)
-            }
-            // row.some(["Ext:", "Name:", "SYNTEL", "Flags"] == row.substring(0, num[i]), characters)
-            // if (row.substr(0, 4) != 'Ext:') {
-            //     if (row.substr(0, 5) != 'Name:') {
-            //         if (row.substr(0, 6) != 'SYNTEL') {
-            //             if (row.substr(0, 5) != 'Flags') {
-            //                 console.log(row)
-            //             }
-            //         }
-            //     }
-            // }
+      for (let i = 0; i < num.length; i++) {
+        if (
+          characters.some((element) => element == row.substring(0, num[i]), row)
+        ) {
+          return true;
         }
       }
+      return false;
+    }
+    let result = {};
+    for (let i = 0; i < rows.length; i++) {
+      let row = rows[i];
+      if (row) {
+        if (!test_String([4, 5, 6],["Ext:", "Name:", "SYNTEL", "----", "Flags"], row)) {
+          console.log(row)
+            if (row.startsWith('Sr No')) {
+              const keys = row.split('|').map(key => key.trim());
+              for (let j = 0; j < keys.length; j++) {
+                result[keys[j]] = [];
+              }
+            } else if (/^\d+$/.test(row.substring(0, row.indexOf(' ')))) {
+              const values = row.split(/ +/).map(value => value.trim());
+              for (let j = 0; j < values.length; j++) {
+                result[Object.keys(result)[j]].push(values[j]);
+              }
+            } else if (row.startsWith('TOTAL(Rs.)')) {
+              break;
+            }
+          
+        }
+      }
+    }
+    console.log(result)
 
-    // for( let i = 0; i < keys.length; i++ ) {
-    //     r[keys[i]] = values[i]
-    // }
-
-    // console.log("remove no string", cleanData())
-    // console.log(rows);
+    let csvContent = "data:text/csv;charset=utf-8,";
+    result.forEach(function(result) {
+      let object = result.join(',');
+      csvContent += object + "\r\n" ;
+    });
+    // console.log("keys to the object", keys)
   };
   fs.readFile("0912.TXT", "utf8", callBackFileRead);
 }
