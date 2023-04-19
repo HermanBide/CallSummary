b56ef871e1d43d15a856b85697c98be0da9e0a82
hermanb <bideherman@yahoo.com>
2023-04-18 19:55:07 -0700

    json to csv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const { type } = require("os");
+// const { parse, jsontocsv } = require("jsontocsv")
 
 function testFileRead() {
   const callBackFileRead = (err, data) => {
@@ -20,37 +21,45 @@ function testFileRead() {
       }
       return false;
     }
+
     let result = {};
     for (let i = 0; i < rows.length; i++) {
       let row = rows[i];
       if (row) {
-        if (!test_String([4, 5, 6],["Ext:", "Name:", "SYNTEL", "----", "Flags"], row)) {
-          console.log(row)
-            if (row.startsWith('Sr No')) {
-              const keys = row.split('|').map(key => key.trim());
-              for (let j = 0; j < keys.length; j++) {
-                result[keys[j]] = [];
-              }
-            } else if (/^\d+$/.test(row.substring(0, row.indexOf(' ')))) {
-              const values = row.split(/ +/).map(value => value.trim());
-              for (let j = 0; j < values.length; j++) {
-                result[Object.keys(result)[j]].push(values[j]);
-              }
-            } else if (row.startsWith('TOTAL(Rs.)')) {
-              break;
+        if (
+          !test_String(
+            [4, 5, 6],
+            ["Ext:", "Name:", "SYNTEL", "----", "Flags"],
+            row
+          )
+        ) {
+          console.log(row);
+          if (row.startsWith("Sr No")) {
+            const keys = row.split("|").map((key) => key.trim());
+            for (let j = 0; j < keys.length; j++) {
+              result[keys[j]] = [];
             }
-          
+          } else if (/^\d+$/.test(row.substring(0, row.indexOf(" ")))) {
+            const values = row.split(/ +/).map((value) => value.trim());
+            for (let j = 0; j < values.length; j++) {
+              result[Object.keys(result)[j]].push(values[j]);
+            }
+          } else if (row.startsWith("TOTAL(Rs.)")) {
+            break;
+          }
         }
       }
     }
-    console.log(result)
+    console.log(result);
 
-    let csvContent = "data:text/csv;charset=utf-8,";
-    result.forEach(function(result) {
-      let object = result.join(',');
-      csvContent += object + "\r\n" ;
-    });
-    // console.log("keys to the object", keys)
+    function convertToCSV(result) {
+      const header = `${Object.keys(result[0]).join("|")}\n`;
+      const rows = result.map(obj => Object.values(obj).join("|")).join("\n");
+      return header + rows;
+    }
+    
+    const csv = convertToCSV(result);
+    console.log(csv);
   };
   fs.readFile("0912.TXT", "utf8", callBackFileRead);
 }
